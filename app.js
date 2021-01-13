7fd5134e38624e56ac33e581818f14e884ce0329
Patrick Hrabos <phrabos@gmail.com>
2021-01-13 16:26:52 -0500

    initialize HTML and JS

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 // import functions and grab DOM elements
+const submitButton = document.getElementById("submit-button")
+const resetButton = document.getElementById("reset-button")
+// const resultsDiv = document.getElementById("results-div")
+const userPickSpan = document.getElementById("user-pick")
+const computerPickSpan = document.getElementById("computer-pick")
+const numResets = document.getElementById("num-resets")
 
 // initialize state
+let resets = 0;
 
 // set event listeners to update state and DOM
+submitButton.addEventListener("click", () =>{
+    
+})
+
+resetButton.addEventListener("click", () =>{
+    
+})
