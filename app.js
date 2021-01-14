34b830a01757b61a8f9730055fc1bbd56b5d471b
Patrick Hrabos <phrabos@gmail.com>
2021-01-13 19:46:02 -0500

    refactor code

--- app.js
+++ app.js
@@ -1,30 +1,11 @@
 // import functions and grab DOM elements
-import {  makeRockPaperScissors  }  from "./utils.js";
+import {  didUserWin, resetGame, computerPick  }  from "./get-random-throw.js";
 const submitButton = document.getElementById("submit-button")
 const resetButton = document.getElementById("reset-button")
-// const resultsDiv = document.getElementById("results-div")
-const userPickSpan = document.getElementById("user-pick")
-const computerPickSpan = document.getElementById("computer-pick")
-const winLoseSpan = document.getElementById("win-lose-span")
-const numResets = document.getElementById("num-resets")
-const winSpan = document.getElementById("win-span")
-const lossSpan = document.getElementById("loss-span")
-const tieSpan = document.getElementById("tie-span")
 
-// initialize state
-export let outcomes = [0,0,0,0];
-let wins = 0;
-let losses = 0;
-let ties = 0;
-let resets = 0;
-// set global variable
-
-let computerPickString = '';
-let computerRandom = 0;
 // set event listeners to update state and DOM
 submitButton.addEventListener("click", () =>{
     const userPick = document.querySelector("input[type=radio]:checked").value;
-
     didUserWin(userPick, computerPick());
 });
 
@@ -32,67 +13,8 @@ resetButton.addEventListener("click", () =>{
     resetGame();
 })
 
-export function computerPick(){
-    computerRandom = Math.floor(Math.random()*3)
-    return computerPickString = makeRockPaperScissors(computerRandom)
-}
 
-export function resetGame() {
-        resets++;
-        numResets.textContent = resets;
-        wins = 0;
-        losses = 0;
-        ties = 0;
-        winSpan.textContent = wins;
-        tieSpan.textContent = ties;
-        lossSpan.textContent = losses;
-    }    
 
-function didUserWin (player, computer){
-    if(player === computer){
-        tie()
-        computerPickSpan.textContent = computer
-        userPickSpan.textContent = player
-    } else if(player === "rock" && computer === 'scissors'){
-        win()
-        userPickSpan.textContent = player
-        computerPickSpan.textContent = computer
-        
-    } else if(player === "rock" && computer === "paper"){
-        loss()
-        userPickSpan.textContent = player
-        computerPickSpan.textContent = computer
-    } else if(player === "paper" && computer === 'rock'){
-        win()
-        userPickSpan.textContent = player
-        computerPickSpan.textContent = computer
-    } else if(player === "paper" && computer === "scissors"){
-        loss()
-        userPickSpan.textContent = player
-        computerPickSpan.textContent = computer
-    } else if(player === "scissors" && computer === 'paper'){
-        win()
-        userPickSpan.textContent = player
-        computerPickSpan.textContent = computer
-    } else if(player === "scissors" && computer === "rock"){
-        loss()
-        userPickSpan.textContent = player
-        computerPickSpan.textContent = computer
-    } 
-}
-function tie(){
-    ties++
-    winLoseSpan.textContent = 'tie'
-    tieSpan.textContent = ties;
-}
-function win(){
-    wins++
-    winLoseSpan.textContent = 'win'
-    winSpan.textContent = wins;
-}
-function loss(){
-    losses++
-    winLoseSpan.textContent = 'lose'
-    lossSpan.textContent = losses
-}
+    
+
 
--- get-random-throw.js
+++ get-random-throw.js
@@ -1,3 +1,77 @@
 import { makeRockPaperScissors } from "./utils.js";
+const userPickSpan = document.getElementById("user-pick")
+const computerPickSpan = document.getElementById("computer-pick")
+const winLoseSpan = document.getElementById("win-lose-span")
+const numResets = document.getElementById("num-resets")
+const winSpan = document.getElementById("win-span")
+const lossSpan = document.getElementById("loss-span")
+const tieSpan = document.getElementById("tie-span")
+// initialize state
+let wins = 0;
+let losses = 0;
+let ties = 0;
+let resets = 0;
+let computerRandom = 0;
+let computerPickString = '';
 
-
+export function didUserWin (player, computer){
+    if(player === computer){
+        tie()
+        computerPickSpan.textContent = computer
+        userPickSpan.textContent = player
+    } else if(player === "rock" && computer === 'scissors'){
+        win()
+        userPickSpan.textContent = player
+        computerPickSpan.textContent = computer
+        
+    } else if(player === "rock" && computer === "paper"){
+        loss()
+        userPickSpan.textContent = player
+        computerPickSpan.textContent = computer
+    } else if(player === "paper" && computer === 'rock'){
+        win()
+        userPickSpan.textContent = player
+        computerPickSpan.textContent = computer
+    } else if(player === "paper" && computer === "scissors"){
+        loss()
+        userPickSpan.textContent = player
+        computerPickSpan.textContent = computer
+    } else if(player === "scissors" && computer === 'paper'){
+        win()
+        userPickSpan.textContent = player
+        computerPickSpan.textContent = computer
+    } else if(player === "scissors" && computer === "rock"){
+        loss()
+        userPickSpan.textContent = player
+        computerPickSpan.textContent = computer
+    } 
+}
+function tie(){
+    ties++
+    winLoseSpan.textContent = 'tie'
+    tieSpan.textContent = ties;
+}
+function win(){
+    wins++
+    winLoseSpan.textContent = 'win'
+    winSpan.textContent = wins;
+}
+function loss(){
+    losses++
+    winLoseSpan.textContent = 'lose'
+    lossSpan.textContent = losses
+}
+export function resetGame() {
+    resets++;
+    numResets.textContent = resets;
+    wins = 0;
+    losses = 0;
+    ties = 0;
+    winSpan.textContent = wins;
+    tieSpan.textContent = ties;
+    lossSpan.textContent = losses;
+}
+export function computerPick(){
+    computerRandom = Math.floor(Math.random()*3)
+    return computerPickString = makeRockPaperScissors(computerRandom)
+}
