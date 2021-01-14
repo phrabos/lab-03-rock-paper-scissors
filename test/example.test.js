140822c3e43f922964758ebd308a5c713f847128
Patrick Hrabos <phrabos@gmail.com>
2021-01-13 20:04:06 -0500

    add tests for functions

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,46 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { makeRockPaperScissors } from '../utils.js';
+import { didUserWin } from '../get-random-throw.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('if zero is entered as arguement then rock will result', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = 'rock';
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const actual = makeRockPaperScissors(0);
 
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });
+test('if 1 is entered as arguement then paper will result', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 'paper';
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = makeRockPaperScissors(1);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+test('if two is entered as arguement then scissors will result', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 'scissors';
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = makeRockPaperScissors(2);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
